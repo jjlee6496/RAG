ecb59c3086da4ab769711c3f4c9e4ec624f1268a
jjlee6496 <ljj3499@gmail.com>
2023-12-18 16:54:04 +0900

    주석 및 가독성 업데이트

--- query_engines/Sentence_window.py
+++ query_engines/Sentence_window.py
@@ -5,7 +5,7 @@ def build_sentence_window_engine_recorder(documents: list, index_dir: str, windo
     """Sentence window 쿼리 엔진과 평가를 위한 recoder를 반환하는 함수
 
     Args:
-        documents (list): parsing을 통해 생성된 chunk를 합친 document
+        documents (list): Document reader를 통해 읽어온 document를 한 리스트로 합친 리스트 객체
         index_dir (str): 인덱스가 저장될 디렉토리
         window_size (int): 윈도우 크기
         app_id (str): 대시보드 로깅을 위해 쓸 app_id. ex)sentence_window 5
@@ -16,11 +16,11 @@ def build_sentence_window_engine_recorder(documents: list, index_dir: str, windo
         
     """
     sentence_index = build_sentence_window_index(
-    documents=documents,
-    llm=OpenAI(model="gpt-3.5-turbo", temperature=0.1),
-    embed_model="local:BAAI/bge-small-en-v1.5",
-    sentence_window_size=window_size,
-    save_dir=index_dir,
+        documents=documents,
+        llm=OpenAI(model="gpt-3.5-turbo", temperature=0.1),
+        embed_model="local:BAAI/bge-small-en-v1.5",
+        sentence_window_size=window_size,
+        save_dir=index_dir,
     )
     
     sentence_window_engine = get_sentence_window_query_engine(
