16c2114272f52dbcd913cd30d303f5651b968aea
jjlee6496 <ljj3499@gmail.com>
2023-12-20 13:35:57 +0900

    전체 실험 진행

--- app.py
+++ app.py
@@ -31,21 +31,53 @@ with open('test.text', 'r') as file:
 basic_rag_query_engine, basic_rag_recorder = build_basic_rag_engine_recorder(
                                                         documents=documents,
                                                         index_dir='./index/test1',
-                                                        app_id='test_app1'
+                                                        app_id='basic RAG'
                                                         )
 
-sentence_window_query_engine, sentence_window_recorder = build_sentence_window_engine_recorder(
+sentence_window_query_engine1, sentence_window_recorder1 = build_sentence_window_engine_recorder(
                                                           documents=documents,
                                                           index_dir='./index/test2',
-                                                          window_size=5,
-                                                          app_id='test_app2'
+                                                          window_size=1,
+                                                          app_id='Window size 1'
                                                         )
 
-auto_merging_query_engine, auto_merging_recorder = build_auto_mergning_engine_recorder(
+sentence_window_query_engine3, sentence_window_recorder3 = build_sentence_window_engine_recorder(
+                                                          documents=documents,
+                                                          index_dir='./index/test3',
+                                                          window_size=3,
+                                                          app_id='Window size 3'
+                                                        )
+
+sentence_window_query_engine5, sentence_window_recorder5 = build_sentence_window_engine_recorder(
+                                                          documents=documents,
+                                                          index_dir='./index/test4',
+                                                          window_size=5,
+                                                          app_id='Window size 5'
+                                                        )
+
+
+auto_merging_query_engine2, auto_merging_recorder2 = build_auto_mergning_engine_recorder(
+                                                    documents=documents,
+                                                    chunk_sizes=[2048, 512],
+                                                    index_dir='./index/test5',
+                                                    app_id='Auto merging 2 Level',
+                                                    similarity_top_k=12,
+                                                    rerank_top_n=6
+                                                    )
+auto_merging_query_engine3, auto_merging_recorder3 = build_auto_mergning_engine_recorder(
                                                     documents=documents,
                                                     chunk_sizes=[2048, 512, 128],
-                                                    index_dir='./index/test3',
-                                                    app_id='testapp3',
+                                                    index_dir='./index/test6',
+                                                    app_id='Auto merging 3 Level',
+                                                    similarity_top_k=12,
+                                                    rerank_top_n=6
+                                                    )
+
+auto_merging_query_engine4, auto_merging_recorder4 = build_auto_mergning_engine_recorder(
+                                                    documents=documents,
+                                                    chunk_sizes=[8192, 2048, 512, 128],
+                                                    index_dir='./index/test7',
+                                                    app_id='Auto merging 4 Level',
                                                     similarity_top_k=12,
                                                     rerank_top_n=6
                                                     )
@@ -53,7 +85,11 @@ auto_merging_query_engine, auto_merging_recorder = build_auto_mergning_engine_re
 # 대시보드 생성
 tru = Tru()
 tru.reset_database()
-# run_evals(eval_questions, basic_rag_recorder, basic_rag_query_engine)
-# run_evals(eval_questions, sentence_window_recorder, sentence_window_query_engine)
-run_evals(eval_questions, auto_merging_recorder , auto_merging_query_engine)
+run_evals(eval_questions, basic_rag_recorder, basic_rag_query_engine)
+run_evals(eval_questions, sentence_window_recorder1, sentence_window_query_engine1)
+run_evals(eval_questions, sentence_window_recorder3, sentence_window_query_engine3)
+run_evals(eval_questions, sentence_window_recorder5, sentence_window_query_engine5)
+run_evals(eval_questions, auto_merging_recorder2 , auto_merging_query_engine2)
+run_evals(eval_questions, auto_merging_recorder3 , auto_merging_query_engine3)
+run_evals(eval_questions, auto_merging_recorder4 , auto_merging_query_engine4)
 tru.run_dashboard()
